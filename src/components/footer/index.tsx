a0d03fb9243d2a4b0d1d0dce9dd4c14cfe9d8f99
Mizuha-hk <hk.haru0803@gmail.com>
2024-04-20 03:51:02 +0900

    [add] 完成見本の作成

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,9 @@
+import "./index.css";
+
+export function Footer() {
+  return (
+    <footer>
+      <p>2024年 超感覚C3講座[Hack] 「自分だけのWebサイトを作ってみよう」</p>
+    </footer>
+  );
+}
---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import "./index.css"
+
+export function Header() {
+  return (
+    <header className="header">
+      <h1>
+        Profile
+      </h1>
+    </header>
+  );
+}
